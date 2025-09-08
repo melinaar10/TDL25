289f84264f53195bd4d2023a6667f5797c8f557b
Melina <melinaarriaran@gmail.com>
2025-09-08 19:28:35 -0300

    no hubo cambios

--- MeliAPractica1/act1.py
+++ MeliAPractica1/act1.py
@@ -1,4 +1,4 @@
 #1-Desarrolla un programa que solicite al usuario que ingrese su edad y luego calcule y muestre cuántos años le faltan para alcanzar los 100 años.
-#edad =int(input("Ingresa tu edad: "))
-#print(type(edad))
-#print ("Le faltan",100-edad,"años para llegar a los 100")
+edad =int(input("Ingresa tu edad: "))
+print(type(edad))
+print ("Le faltan",100-edad,"años para llegar a los 100")
